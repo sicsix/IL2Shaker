e78d2bba8bf3c86e5a3466c79ae289748d219c3d
sicsix <8847273+sicsix@users.noreply.github.com>
2023-07-10 18:04:26 +1000

    Gunfire reworked.
    Now uses a single frequency with no dropoff.
    Multiple guns firing simultaneously has diminishing returns.
    Tightened the frequency band.
    Using the sixth root of the kinetic energy to select amplitude and frequency.
    End result is everything feels punchier but big still feels bigger.

--- IL2ShakerDriver/DB/Gun.cs
+++ IL2ShakerDriver/DB/Gun.cs
@@ -2,11 +2,11 @@
 
 internal record struct Gun(string Name, float RPM, float Velocity, float Mass)
 {
-    public readonly string Name                = Name;
-    public readonly float  RPM                 = RPM;
-    public readonly float  Velocity            = Velocity;
-    public readonly float  Mass                = Mass;
-    public readonly float  KineticEnergy       = 0.5f * Mass * Velocity * Velocity / 1000f;
-    public readonly float  SqrKineticEnergy    = MathF.Sqrt(0.5f                  * Mass * Velocity * Velocity / 1000f);
+    public readonly string Name            = Name;
+    public readonly float  RPM             = RPM;
+    public readonly float  Velocity        = Velocity;
+    public readonly float  Mass            = Mass;
+    public readonly float  KineticEnergy   = 0.5f * Mass * Velocity * Velocity / 1000f;
+    public readonly float  RtKineticEnergy = MathF.Pow(0.5f * Mass * Velocity * Velocity / 1000f, 1 / 6f);
     public readonly int    SamplesBetweenShots = (int)(SimClock.SampleRate / (RPM / 60f));
 }
