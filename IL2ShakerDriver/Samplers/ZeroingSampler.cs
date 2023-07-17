6c74d6513ffdfa3b3711e1210298e50529e4e9fb
sicsix <8847273+sicsix@users.noreply.github.com>
2023-07-18 08:03:05 +1000

    Multichannel routing added.

---
+++ IL2ShakerDriver/Samplers/ZeroingSampler.cs
@@ -0,0 +1,23 @@
+﻿using NAudio.Wave;
+
+namespace IL2ShakerDriver.Samplers;
+
+public class ZeroingSampler : ISampleProvider
+{
+    public WaveFormat WaveFormat { get; }
+
+    private readonly ISampleProvider _source;
+
+    public ZeroingSampler(ISampleProvider source)
+    {
+        WaveFormat = source.WaveFormat;
+        _source    = source;
+    }
+
+    public int Read(float[] buffer, int offset, int count)
+    {
+        Array.Clear(buffer);
+
+        return _source.Read(buffer, offset, count);
+    }
+}
